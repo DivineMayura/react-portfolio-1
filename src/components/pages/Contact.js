33c0f1e926820ad7738a552cc3fe186f6eeb88d7
May <MayyFaucher@gmail.com>
2021-11-23 21:58:30 -0800

    removed list in contact.js, and I'm instead making it buttons

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,71 +4,70 @@ import React from 'react';
 
 
 export default function contact() {
-
-
-    function handleFormSubmit() {
-
-
-    //     return(  <div className={`alert alert-success'}`} role="alert">"horse"</div>
-    // ) 
-        alert("This feature hasn't been implemented yet. \nInstead you can email me at: MayyFaucher@gmail.com");
-    }
-
-
-
-
-
+    
+    // This throws an error when I import useState from react for some reason.
+    // const [email, setEmail] = useState('');
+    // const [name, setName] = useState('');
 
 
 
 
     return (
         <div className="padding flexContact">
-            <ul className="contactList">
-                <li className="contactItem">
+            <h5 className="mayAlignTextCenter">
+                <button className="contactButton">
                     <a href="https://github.com/DivineMayura"
                         rel="noreferrer"
                         target="_blank"
                         className="contactLink">GitHub</a>
-                </li>
-                <li className="contactItem">
+                </button>
+            </h5>
+            <h5 className="mayAlignTextCenter">
+                <button className="contactButton">
                     <a href="https://www.linkedin.com/in/mayfaucher/"
                         rel="noreferrer"
                         target="_blank"
-                        className="contactLink">LinkedIn</a>
-                </li>
-                <li className="contactItem">
+                        className="contactLink nav-link">LinkedIn</a>
+                </button>
+
+            </h5>
+            <h5 className="mayAlignTextCenter">
+                <button className="contactButton">
                     <a href="https://medium.com/@mayyfaucher"
                         rel="noreferrer"
                         target="_blank"
                         className="contactLink">Medium.com</a>
-                </li>
-            </ul>
+                </button>
+            </h5>
             <h5 className="mayAlignTextCenter">
-                <a href="./../../Resume-May.docx" download>download resume</a>
+                <button className="contactButton">
+                    <a href="./../../Resume-May.docx" download>download resume</a></button>
             </h5>
             <hr />
             <div>
                 <div>
                     <form className="form">
                         <input
-                            // onChange={orThisWayForBoth}
+                            // value={name}
+                            // onChange={handleInputChange}
                             type="text"
-                            placeholder="Name"
-                        />
+                            placeholder="Name" />
                         <br />
                         <input
-                            // onChange={orThisWayForBoth}
-                            type="text"
-                            placeholder="Email"
-                        />
+                            // value={email}
+                            // onChange={handleInputChange}
+                            type="email"
+                            placeholder="Email" />
                         <br />
                         <br />
                         <textarea placeholder="Email Content"></textarea>
                         <hr />
-                        <button type="button" onClick={handleFormSubmit}>
+                        <button type="button">
                             Submit
                         </button>
+                        <div className="formSubmitHidden">
+                            This Feature Hasn't fully been implemented yet. You can instead email me here at: MayyFaucher@gmail.com .
+                        </div>
                     </form>
                 </div>
             </div>
